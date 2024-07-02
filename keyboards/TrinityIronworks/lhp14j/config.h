0e2fb6900324817f196b884ca51c94946562bf3d
knhouse-32 <knhouse32@gmail.com>
2024-07-02 19:28:56 +0900

    make lhp14:vil

---
+++ keyboards/TrinityIronworks/lhp14j/config.h
@@ -0,0 +1,16 @@
+// Copyright 2023 Neo Trinity
+// SPDX-License-Identifier: GPL-2.0-or-later
+#define VIAL_KEYBOARD_UID {0x3B, 0x0B, 0x38, 0x93, 0x6E, 0xCB, 0x0E, 0xB5}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+#pragma once
+
+#define JOYSTICK_BUTTON_COUNT 1
+#define JOYSTICK_AXIS_COUNT 2
+
+#define OLED_TIMEOUT 0
+
+#define OLED_FONT_H "keyboards/lhp14j/glcdfont_lhp14.c"
+
+#define LAYER_STATE_32BIT
---
+++ keyboards/TrinityIronworks/lhp14j/config_org.h
@@ -0,0 +1,13 @@
+// Copyright 2023 Neo Trinity
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define JOYSTICK_BUTTON_COUNT 1
+#define JOYSTICK_AXIS_COUNT 2
+
+#define OLED_TIMEOUT 0
+
+#define OLED_FONT_H "keyboards/lhp14j/glcdfont_lhp14.c"
+
+#define LAYER_STATE_32BIT
